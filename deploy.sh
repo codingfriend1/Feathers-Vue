f24a4c0c399663e4e29c2331662ea0843cf16c10
Jon Paul Miles <codingfriend1@gmail.com>
2017-08-07 20:04:15 +0000

    Updates deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-sshpass -p $1 ssh -o StrictHostKeyChecking=no $3@$2 '
-	echo $1
-	echo $2
-	echo $3
-	echo $4
-	echo $5
-	echo $6
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+sshpass -p $1 ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o PubkeyAuthentication=no $3@$2 '
 	docker login -u gitlab-ci-token -p $4 registry.gitlab.com
 	docker-compose stop
 	docker-compose rm
