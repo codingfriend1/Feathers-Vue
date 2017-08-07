6f709ac58be53716d89546a5f50b4b9d68994e8a
Jon Paul Miles <codingfriend1@gmail.com>
2017-08-07 20:27:57 +0000

    Adds logs.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ echo $5
 echo $6
 echo $7
 sshpass -p $1 ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o PubkeyAuthentication=no $3@$2 '
+	echo "inside digitalocean"
+	echo $4
+	echo $5
 	docker login -u $4 -p $5 registry.gitlab.com
 	docker-compose stop
 	docker-compose rm
