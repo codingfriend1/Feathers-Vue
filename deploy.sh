b7cc24489ee8b241bed1041611b8c27ea5ec2b56
Jon Paul Miles <codingfriend1@gmail.com>
2017-08-07 21:34:59 +0000

    Fixes single quotes to double quotes for env variables.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,14 @@
 echo $1
 echo $2
 echo $3
-echo $4
-echo $5
-echo $6
-echo $7
-sshpass -p $1 ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o PubkeyAuthentication=no $3@$2 '
-	echo "inside digitalocean"
-	echo $4
-	echo $5
-	docker login -u $4 -p $5 registry.gitlab.com
+DOCKER_USER=$4
+DOCKER_PASS=$5
+GITLAB_REGISTRY=$6
+GITLAB_REPO=$7
+sshpass -p $1 ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o PubkeyAuthentication=no $3@$2 "
+	docker login -u $DOCKER_USER -p $DOCKER_PASS $GITLAB_REGISTRY
 	docker-compose stop
 	docker-compose rm
-	docker pull $6/$7
+	docker pull $GITLAB_REGISTRY/$GITLAB_REPO
 	docker-compose up -d
-'
+"
