b20bf91591fab7a63e7792ae465969b679026cd8
Jon Paul Miles <codingfriend1@gmail.com>
2017-08-07 21:37:51 +0000

    Names other arugments.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-echo $1
-echo $2
-echo $3
+SERVER_PASS=$1
+SERVER_IP=$2
+SERVER_USER=$3
 DOCKER_USER=$4
 DOCKER_PASS=$5
 GITLAB_REGISTRY=$6
 GITLAB_REPO=$7
-sshpass -p $1 ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o PubkeyAuthentication=no $3@$2 "
+sshpass -p $SERVER_PASS ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o PubkeyAuthentication=no $SERVER_USER@$SERVER_IP "
 	docker login -u $DOCKER_USER -p $DOCKER_PASS $GITLAB_REGISTRY
 	docker-compose stop
 	docker-compose rm
