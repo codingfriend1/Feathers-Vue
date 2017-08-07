48fa9c6542c122e39d40886beb4c11f17d9d1616
Jon Paul Miles <codingfriend1@gmail.com>
2017-08-07 19:45:43 +0000

    Echoes variables.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
-sshpass  -p $1 ssh -o StrictHostKeyChecking=no $3@$2 <<-'ENDSSH'   
-   docker login -u gitlab-ci-token -p $4 registry.gitlab.com
-   docker-compose stop
-   docker-compose rm
-   docker pull $5/$5
-   docker-compose up -d
+sshpass  -p $1 ssh -o StrictHostKeyChecking=no $3@$2 <<-'ENDSSH'
+	echo $1
+	echo $2
+	echo $3
+	echo $4
+	echo $5
+	echo $6
+	docker login -u gitlab-ci-token -p $4 registry.gitlab.com
+	docker-compose stop
+	docker-compose rm
+	docker pull $5/$6
+	docker-compose up -d
 ENDSSH
