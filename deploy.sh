751b8537d5f3c9aa0cf4bcb480503a539722574a
Jon Paul Miles <codingfriend1@gmail.com>
2017-08-07 18:27:49 +0000

    Run deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-sshpass  -p $1 ssh -o StrictHostKeyChecking=no root@$2 <<-'ENDSSH'   
-   docker login -u gitlab-ci-token -p $3 registry.gitlab.com
+sshpass  -p $1 ssh -o StrictHostKeyChecking=no $3@$2 <<-'ENDSSH'   
+   docker login -u gitlab-ci-token -p $4 registry.gitlab.com
    docker-compose stop
    docker-compose rm
-   docker pull $4/$5
+   docker pull $5/$5
    docker-compose up -d
 ENDSSH
