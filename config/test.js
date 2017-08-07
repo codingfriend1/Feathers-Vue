3eadc50d0614413336fa69bab314b226368aab63
Jon Paul Miles <codingfriend1@gmail.com>
2017-08-07 05:20:53 +0000

    Adds separate test env variable for mongo.

--- config/test.js
+++ config/test.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "mongodb": process.env.DATABASE_URL || "mongodb://localhost:27017/FeathersVueTest"
+  "mongodb": process.env.DATABASE_URL || process.env.DATABASE_TEST_URL || "mongodb://localhost:27017/FeathersVueTest"
 }
