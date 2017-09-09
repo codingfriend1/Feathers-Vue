819360b9ded0fa9194bb2caa06fe568def3f15fa
Jon Paul Miles <codingfriend1@gmail.com>
2017-09-09 19:52:44 +0000

    Better separates idfield.

--- server/hooks/associate-current-user.js
+++ server/hooks/associate-current-user.js
@@ -6,15 +6,23 @@ const { getItems } = require('feathers-hooks-common');
 const _ = require('lodash');
 const errors = require('feathers-errors');
 
-module.exports = function (fieldToSet = 'userId') { // eslint-disable-line no-unused-vars
+const defaults = {
+  idField: '_id',
+  fieldToSet: 'userId'
+};
+
+module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
+
+  const settings = Object.assign({}, defaults, options)
+
   return function (hook) {
 
-  	if(!_.get(hook, 'params.user._id')) {
+  	if(!_.get(hook, `params.user[${settings.idField}]`)) {
   		throw new errors.NotAuthenticated(`The current user is missing. You must not be authenticated.`);
   	}
 
   	( _.castArray( getItems(hook) ) )
-  	  .forEach(item => { item[fieldToSet] = _.get(hook, 'params.user._id'); });
+  	  .forEach(item => { item[settings.fieldToSet] = _.get(hook, `params.user[${settings.idField}]`); });
 
     // Hooks can either return nothing or a promise
     // that resolves with the `hook` object for asynchronous operations
